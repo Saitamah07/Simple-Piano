4f2d309b119052aa86f43ceae42f5d0bf621f460
Saitam <matias279ah@gmail.com>
2023-12-16 12:15:29 -0500

    Initial commit of the Simple-Piano

---
+++ index.js
@@ -0,0 +1,65 @@
+// Select all elements with the class 'drum' and store them in the 'buttons' variable
+const buttons = document.querySelectorAll('.drum');
+
+// Add a click event listener to each button
+for (const button of buttons) {
+	button.addEventListener('click', function () {
+		// Get the inner HTML (text) of the clicked button
+		const buttonInnerHtml = this.innerHTML;
+
+		// Call the makeSound function with the inner HTML as an argument
+		makeSound(buttonInnerHtml);
+
+		// Call the buttonAnimation function with the inner HTML as an argument
+		buttonAnimation(buttonInnerHtml);
+	});
+}
+
+// Detect keyboard keydown event
+document.addEventListener('keydown', function (event) {
+	// Call the makeSound function with the pressed key as an argument
+	makeSound(event.key);
+
+	// Call the buttonAnimation function with the pressed key as an argument
+	buttonAnimation(event.key);
+});
+
+// Function to play a sound based on the provided key
+function makeSound(key) {
+	// Define a mapping between keys and sound file names
+	const soundMappings = {
+		q: 'do.wav',
+		w: 'fa.wav',
+		e: 'la.wav',
+		r: 'mi.wav',
+		t: 're.wav',
+		y: 'si.wav',
+		u: 'sol.wav',
+		i: 're.wav',
+		o: 'si.wav',
+		p: 'sol.wav',
+	};
+
+	// Get the file name from the mapping based on the provided key
+	const fileName = soundMappings[key];
+
+	// If a file name is found, create an Audio object and play the corresponding sound
+	if (fileName) {
+		const audio = new Audio(`./sounds/${fileName}`);
+		audio.play();
+	}
+}
+
+// Function to add a temporary 'pressed' class for animation to the clicked or pressed button
+function buttonAnimation(currentKey) {
+	// Select the element with the class matching the current key
+	const activeButton = document.querySelector('.' + currentKey);
+
+	// Add the 'pressed' class to the selected button
+	activeButton.classList.add('pressed');
+
+	// Set a timeout to remove the 'pressed' class after 100 milliseconds
+	setTimeout(function () {
+		activeButton.classList.remove('pressed');
+	}, 100);
+}
