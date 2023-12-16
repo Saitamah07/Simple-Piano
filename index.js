0207b090521dd1d31d6785f116a41f1c4f595321
Saitam <matias279ah@gmail.com>
2023-12-16 12:33:32 -0500

    change to only Keydown

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// Select all elements with the class 'drum' and store them in the 'buttons' variable
-const buttons = document.querySelectorAll('.drum');
-
-// Add a click event listener to each button
-for (const button of buttons) {
-	button.addEventListener('click', function () {
-		// Get the inner HTML (text) of the clicked button
-		const buttonInnerHtml = this.innerHTML;
-
-		// Call the makeSound function with the inner HTML as an argument
-		makeSound(buttonInnerHtml);
-
-		// Call the buttonAnimation function with the inner HTML as an argument
-		buttonAnimation(buttonInnerHtml);
-	});
-}
-
 // Detect keyboard keydown event
 document.addEventListener('keydown', function (event) {
 	// Call the makeSound function with the pressed key as an argument
